07e66febf3ae854c02896638bd1d119ac8309383
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-09-04 00:26:32 -0400

    vamos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,14 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+import path from 'node:path';
+
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@': path.resolve(process.cwd(), 'src'),
+                '@cv': path.resolve(process.cwd(), 'src/cv.json'),
+            },
+        },
+    },
+});
